eba0a525c02c9ba8541a71ab3bd48fdcb17c1c86
taotaotheripper <taotaotheripper@gmail.com>
2013-02-01 17:10:47 +0000

    Change names of authorize module.

---
+++ auth_config.php
@@ -0,0 +1,7 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define("WB_AKEY", '2359821431');
+define("WB_SKEY", 'cb09302d0e1f0bd236075f2c6b777073');
+define("WB_CALLBACK_URL", 'http://127.0.0.1/prophet-pku-eecs/auth_cb.php');
+?>
